dcc9adc1c485a223d971946d40aba5360d870c03
ericgerzen <ericyes81007@gmail.com>
2025-09-22 12:05:49 -0300

    textures

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ clock = pygame.time.Clock()
 
 tile_size = 50
 
+grass_img = pygame.image.load("assets/grass.png")
+sand_img = pygame.image.load("assets/sand.png")
+spawn_img = pygame.image.load("assets/spawn.png")
+
 #Draw Grid Function
 def draw_grid():
-
-    for line in range(0, (screen_width - 100) // tile_size + 1):
-        x = 100 + line * tile_size
+    x_offset = 100
+    for line in range(0, (screen_width - x_offset) // tile_size + 1):
+        x = x_offset + line * tile_size
         pygame.draw.line(screen, (0, 0, 0), (x, 0), (x, screen_height))
 
     for line in range(0, screen_height // tile_size + 1):
         y = line * tile_size
-        pygame.draw.line(screen, (0, 0, 0), (100, y), (screen_width, y))
+        pygame.draw.line(screen, (0, 0, 0), (x_offset, y), (screen_width, y))
 
 world_data = [
     [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -46,25 +50,41 @@ class World:
 
         for row in range(len(data)):
             for col in range(len(data[row])):
-                color = None
+                x_offset = 100  # Sidebar width
                 if data[row][col] == 0:
-                    color = (0, 0, 255)
-                elif data[row][col] == 1:
-                    color = (255, 0, 0)
-                elif data[row][col] == 2:
-                    color = (255, 255, 255)
-                elif data[row][col] == 3:
-                    color = (0, 255, 0)
-                if color:
-                    tile_surface = pygame.Surface((tile_size, tile_size))
-                    tile_surface.fill(color)
-                    tile_rect = tile_surface.get_rect()
-                    tile_rect.topleft = (100 + col * tile_size, row * tile_size)
-                    self.tile_list.append((tile_surface, tile_rect))
+                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = x_offset + col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if data[row][col] == 1:
+                    img = pygame.transform.scale(sand_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = x_offset + col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if data[row][col] == 2:
+                    img = pygame.transform.scale(spawn_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = x_offset + col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                if data[row][col] == 3:
+                    img = pygame.Surface((tile_size, tile_size))
+                    img.fill((255, 0, 0))  # Red
+                    img_rect = img.get_rect()
+                    img_rect.x = x_offset + col * tile_size
+                    img_rect.y = row * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+
 
     def draw(self):
-        for tile_surface, tile_rect in self.tile_list:
-            screen.blit(tile_surface, tile_rect)
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
 
 world = World(world_data)
 
